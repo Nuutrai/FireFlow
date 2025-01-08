0fd10d8b067fd0579e93b5952f7f693a4f62f5ce
Lexi Nuutraî <info@nuutrai.com>
2025-01-08 11:55:54 -0500

    Suggestions Merge Cancel and Uncancel event

--- src/main/java/de/blazemcworld/fireflow/code/node/impl/action/CancelEventNode.java
+++ src/main/java/de/blazemcworld/fireflow/code/node/impl/action/CancelEventNode.java
@@ -1,6 +1,7 @@
 package de.blazemcworld.fireflow.code.node.impl.action;
 
 import de.blazemcworld.fireflow.code.node.Node;
+import de.blazemcworld.fireflow.code.type.ConditionType;
 import de.blazemcworld.fireflow.code.type.SignalType;
 import net.minestom.server.event.Event;
 import net.minestom.server.event.trait.CancellableEvent;
@@ -12,12 +13,21 @@ public class CancelEventNode extends Node {
         super("cancel_event", Material.BARRIER);
 
         Input<Void> signal = new Input<>("signal", SignalType.INSTANCE);
+        Input<Boolean> cancel = new Input<>("cancel", ConditionType.INSTANCE);
         Output<Void> next = new Output<>("next", SignalType.INSTANCE);
+        Output<Void> failed = new Output<>("failed", SignalType.INSTANCE);
         signal.onSignal((ctx) -> {
             Event event = ctx.event;
-            if (event instanceof CancellableEvent e) {
-                e.setCancelled(true);
+            boolean cancels = cancel.getValue(ctx);
+            if (!(event instanceof CancellableEvent e)) {
+                ctx.sendSignal(failed);
+                return;
             }
+            if (e.isCancelled() == cancels) {
+                ctx.sendSignal(failed);
+                return;
+            }
+            e.setCancelled(cancels);
             ctx.sendSignal(next);
         });
     }
